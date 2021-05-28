4404ac394eb13e938f5bf871433406e0e132e943
crosgar <cecilia.rossi.garcia@gmail.com>
2021-05-28 14:36:27 -0700

    added app.post to create and add new animals to animals array, added response validation

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
    let personalityTraitsArray = [];
@@ -37,6 +44,33 @@ function findById(id, animalsArray) {
    return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+   const animal = body;
+   animalsArray.push(animal);
+   fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({animals: animalsArray}, null, 2)
+   );
+
+   return animal;
+}
+
+function validateAnimal(animal) {
+   if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+   }
+   if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+   }
+   if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+   }
+   if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+   }
+   return true;
+};
+
 app.get('/api/animals', (req, res) => {
    let results = animals;
    if (req.query) {
@@ -51,6 +85,21 @@ app.get('/api/animals/:id', (req,res) => {
       res.json(result);
    } else {
       res.sendStatus(404);
+   }
+});
+
+app.post('/api/animals', (req, res) => {
+   // set id based on what the next index will be
+   req.body.id = animals.length.toString();
+
+   //validate data
+   if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+   } else {
+     //add animal to json file and animals array
+     const animal = createNewAnimal(req.body, animals);
+     //req.body is where incoming content will be
+     res.json(animal);
    }
 });
 
